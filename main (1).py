5bf7c8fc4d02c0656d78c05c61d4424626f28a33
Kanmanisubramaniyan05 <144817031+Kanmanisubramaniyan05@users.noreply.github.com>
2023-09-12 22:38:26 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,23 @@
+# leaf year
+
+"""
+
+
+
+
+"""
+def isleafyear(year):
+  if(year % 4 == 0 and year % 100 !=0):
+    return True
+  else:
+    return False
+
+year = int(input("Enter a year:"))
+
+if isleafyear(year):
+  print('{} is not leaf year.'.format(year))
+
+
+
+    
+  
